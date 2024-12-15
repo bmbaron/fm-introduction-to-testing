408183f7ea48e6eac83c869bfd4b634443be1185
bmbaron <benjamin.m.baron@gmail.com>
2024-12-15 15:42:15 +0700

    init testing

--- examples/basic-math/src/arithmetic.js
+++ examples/basic-math/src/arithmetic.js
@@ -1,7 +1,24 @@
-export const add = () => {};
+export const add = (a, b) => {
+	if (typeof a === 'string') a = Number(a);
+	if (typeof b === 'string') b = Number(b);
+	if (isNaN(a) || isNaN(b)) throw new Error('One of the inputs is not a number');
+	return a + b;
+};
 
-export const subtract = () => {};
+export const subtract = (a, b) => {
+	if (Array.isArray(a)) {
+		a = a.reduce((a, b) => a - b);
+	}
+	if (Array.isArray(b)) {
+		b = b.reduce((a, b) => a - b);
+	}
+	return a - b;
+};
 
-export const multiply = () => {};
+export const multiply = (a, b) => {
+	return a * b;
+};
 
-export const divide = () => {};
+export const divide = (a, b) => {
+	return a / b;
+};
--- examples/basic-math/src/arithmetic.test.js
+++ examples/basic-math/src/arithmetic.test.js
@@ -1,9 +1,38 @@
 import { describe, it, expect } from 'vitest';
+import { add, subtract, multiply, divide } from './arithmetic.js';
 
-describe.todo('add', () => {});
+describe('add', () => {
+	it('should add two numbers together', () => {
+		expect(add(2,2)).toBe(4);
+	});
+	it('should parse strings into numbers', () => {
+		expect(add('1', '1')).toBe(2);
+	})
+	it('should throw errors if inputs are not number', () => {
+		expect(() => add(2, 'potato')).toThrow('not a number');
+	})
+});
 
-describe.todo('subtract', () => {});
+describe('subtract', () => {
+	it('should subtract the second number from the first number', () => {
+		expect(subtract(4, 2)).toBe(2);
+	});
 
-describe.todo('multiply', () => {});
+	it('should accept and attract all the numbers in an array', () => {
+		expect(subtract([10, 5], 2)).toBe(3);
+		expect(subtract([10, 5, 1], 2)).toBe(2);
+	});
 
-describe.todo('divide', () => {});
+});
+
+describe('multiply', () => {
+	it('should multiple two numbers together', () => {
+		expect(multiply(2, 3)).toBe(6);
+	});
+});
+
+describe('divide', () => {
+	it('should divide the first number by the second number', () => {
+		expect(divide(4, 2)).toBe(2);
+	});
+});
